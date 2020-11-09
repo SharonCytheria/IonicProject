e3f98c4ad4bf2c8752bc411966134ae7e6c27ad1
SharonCytheria <46226635+SharonCytheria@users.noreply.github.com>
2020-11-09 16:40:04 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,10 +21,17 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     StatusBar,
+    Camera,
+    BarcodeScanner,
+    ImagePicker,
+    /*
+    Type 'BarcodeScannerOriginal' is not assignable to type 'Provider'.
+    Type 'BarcodeScannerOriginal' is missing the following properties from type 'FactoryProvider': provide, useFactoryts(2322)
+    */
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     LocalStorageService
--- src/app/routes/Product/product-add/product-add.page.ts
+++ src/app/routes/Product/product-add/product-add.page.ts
@@ -5,6 +5,10 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/shared/class/product';
 import { Router } from '@angular/router';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Camera } from '@ionic-native/camera/ngx'
+import { ImagePicker } from '@ionic-native/image-picker/ngx'
+import { StatusBar } from '@ionic-native/status-bar/ngx'
 
 @Component({
   selector: 'app-product-add',
@@ -21,6 +25,10 @@ export class ProductAddPage implements OnInit, OnDestroy{
     private router: Router,
     private zone: NgZone,
     private categoryService: CategoryListServiceService,
+    private barcodeScanner: BarcodeScanner, 
+    private camera: Camera,
+    private imagePicker: ImagePicker,
+    private statusBar: StatusBar,
   ) { 
     this.subscription = categoryService.watchCategory().subscribe(
       (activeCategory)=> {
@@ -30,7 +38,29 @@ export class ProductAddPage implements OnInit, OnDestroy{
     });
     this.initiateProduct();
     this.product.categoryName="默认分类";
+    //---------camera
+    const options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,'+ imageData;
+    }, (err) => {
+      //Handle error
+    });
+    //---------imagePicker
+    this.imagePicker.getPictures(options).then((results) => {
+      for( var i = 0; i < results.length; i++) {
+        console.log('Image URI: '+ results[i]);
+      }
+    }, (err) => { })
+    //---------statusBar
+    this.statusBar.overlaysWebView(true);
   }
+
+  
 
   ngOnInit() {
   }
@@ -102,4 +132,20 @@ export class ProductAddPage implements OnInit, OnDestroy{
   GoToCategoryList(){
     this.router.navigateByUrl("/category-list");
   }
+  // 使用Barcode Scanner插件实现二维码、条形码扫描
+  onScan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      // others
+      alert(JSON.stringify(barcodeData));
+    }).catch(err => {
+      console.log('Error', err);
+      alert(err);
+    });
+  }
+  /*
+  JSON.stringify() 方法用于将 JavaScript 值转换为 JSON 字符串。
+  */
+
+
 }
